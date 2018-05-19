48d04e549009c214603125e9d1506fe5ff53a64c
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-05-20 00:35:47 +0800

    sell a product front-end after sign-up

--- client/app/components/Sell/Sell.js
+++ client/app/components/Sell/Sell.js
@@ -155,7 +155,7 @@ class Sell extends Component {
           signUpLocation,
         } = this.state;
 
-        console.log(token);
+
 
         if (!token) {
             return (
@@ -213,7 +213,49 @@ class Sell extends Component {
 
         return (
             <div>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><h1>logged-in</h1>
+              <main className="main">
+                  <div className="sell-wrapper">
+                    <h1 className="circular_black_font">Sell your clothes</h1>
+                    <h3 className="circular_font"> Add photos of your product </h3>
+                    <div className="photo-list">
+                      <div className="center-cropped photo-item" style={{backgroundColor: 'gray', display: 'inline-block'}}>
+                        <img src={require("../../../public/assets/img/items.jpg")} className="prodlist_item_thumbnail"/>
+                      </div>
+                      <div className="center-cropped photo-item" style={{backgroundColor: '#BBBBBB', display: 'inline-block'}}>
+                        <i className="material-icons" style={{margin: 'auto', fontSize: '100px', textAlign: 'center', alignContent: 'center', width: '100%', lineHeight: '200px'}}>camera_alt</i>
+                      </div>
+                    </div>
+
+                    <form action="#">
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input circular_font" type="text" id="title"/>
+                        <label className="mdl-textfield__label circular_font" htmlFor="title">Name of your clothing</label>
+                      </div>
+                      <br/>
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input circular_font" type="text" id="type"/>
+                        <label className="mdl-textfield__label circular_font" htmlFor="type">Category</label>
+                      </div>
+                      <br/>
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input circular_font" type="text" id="prov"/>
+                        <label className="mdl-textfield__label circular_font" htmlFor="prov">Province</label>
+                      </div>
+                      <br/>
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input circular_font" type="text" id="bar"/>
+                        <label className="mdl-textfield__label circular_font" htmlFor="bar">City</label>
+                      </div>
+                      <br/>
+                      <div className="mdl-textfield mdl-js-textfield">
+                        <input className="mdl-textfield__input circular_font" type="number" id="price"/>
+                        <label className="mdl-textfield__label circular_font" htmlFor="price">Price</label>
+                      </div>
+                      <br/><br/>
+                      <input className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" value="Submit"/>
+                    </form>
+                  </div>
+              </main>
             </div>
         );
     }
